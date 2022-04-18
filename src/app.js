98617a1377441dd86dbeb3a357a2d99572bd01fc
zhangbing <zhangb@mctech.vip>
2022-04-18 13:28:35 +0800

    修改cli方式执行错误

--- src/app.js
+++ src/app.js
@@ -1,4 +1,22 @@
 #!/usr/bin/env node
 
-// copy to dist
-import './cli.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { exec } from 'child_process'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const args = process.argv
+  .slice(2)
+  .map(a => '"' + a + '"')
+  .join(' ')
+
+const cliPath = path.join(__dirname, 'cli.js')
+exec(
+  `node  --experimental-import-meta-resolve --experimental-vm-modules "${cliPath}" ${args}`,
+  function (err, stdout, stderr) {
+    if (err) throw err
+    console.log(stdout)
+  }
+)
