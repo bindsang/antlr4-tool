178413762864e44ba04a4f5fb7bf1ab66b559f5a
zhangbing <zhangb@mctech.vip>
2023-02-27 12:36:24 +0800

    antlr4版本号根据当前添加的依赖包动态生成

--- src/antlr-core/constants.js
+++ src/antlr-core/constants.js
@@ -1,13 +1,49 @@
+import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-export const ANTLR_VERSION = '4.11.0'
+export const ANTLR_VERSION = getAntlr4Version()
 export const ANTLR_JAR = path.join(
     __dirname,
     '..',
     'bin',
     `antlr-${ANTLR_VERSION}-complete.jar`
 )
+
+function getAntlr4Version () {
+    let currentDir = __dirname
+    let mainDir
+    while (true) {
+        const fileName = path.join(currentDir, 'package.json')
+        if (fs.existsSync(fileName)) {
+            mainDir = currentDir
+            break
+        }
+        const parentDir = path.dirname(currentDir)
+        if (!parentDir || parentDir === currentDir) {
+            break
+        }
+        currentDir = parentDir
+    }
+
+    if (!mainDir) {
+        throw new Error('未找到主模块入口目录')
+    }
+
+    const fileName = path.join(
+        mainDir,
+        'node_modules',
+        'antlr4',
+        'package.json'
+    )
+    if (fs.existsSync(fileName)) {
+        const content = fs.readFileSync(fileName, 'utf8')
+        const pkg = JSON.parse(content)
+        return pkg.version
+    }
+
+    throw new Error('未找到当前使用的antlr4依赖包')
+}
